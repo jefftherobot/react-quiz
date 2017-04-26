4ba47d63bcf46150a4ae071681229df1d1efbd41
Adrienne McDonnell <amcdonnell@delphicdigital.com>
2017-04-26 15:32:02 -0400

    update results page with mock api data

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import FormOption from '../components/FormOption';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import ResultItem from '../components/ResultItem';
+
+function showLoans() {
+	const button = document.getElementById('result__button'),
+		  resultList = document.getElementById('result__list');
+
+	if (resultList.classList.contains('result__list--collapsed')) {
+		button.textContent = 'See Less Loan Options';
+		resultList.classList.remove('result__list--collapsed');
+	} else {
+		button.textContent = 'See More Loan Options';
+		resultList.classList.add('result__list--collapsed');
+	}
+}
 
 function Result(props) {
+	let counter = 0;
 	const resultObj = props.quizResult;
 	console.log(resultObj);
 
-	/*const results = Object.keys(resultObj).map(function(key) {
-		return <li className="results__item" key={key}>{key}: {resultObj[key]}</li>
+	/*const result = Object.keys(resultObj).map(function(key) {
+		return <li className="result__item" key={key}>{key}: {resultObj[key]}</li>
 	});*/
+
+	function renderResultItems(key) {
+		counter++
+
+		return (
+			<ResultItem 
+				key={'loan' + counter}
+				id={'loan' + counter}
+				loan={props.quizResult[counter - 1]}
+				loanName={props.quizResult[counter - 1].name}
+				loanRate={props.quizResult[counter - 1].rate}
+				loanPayment={props.quizResult[counter - 1].monthlyPayment}
+				loanAPR={props.quizResult[counter - 1].apr}
+				loanFees={props.quizResult[counter - 1].lenderFees}
+			/>
+		);
+	}
 
 	return (
 		<div>
-			<ul className="results__list">
-				{/* results */ }
+			<QuestionCount counter={props.questionId} total={props.questionTotal} progress={props.progress}/>
+			<Question content={props.question} />
+
+			<h1>Your personalized rate quote:</h1>
+			<ul id="result__list" className="result__list result__list--collapsed">
+				{props.quizResult.map(renderResultItems)}
 			</ul>
+
+			<button id="result__button" className="result__button" onClick={showLoans}>See More Loan Options</button>
 		</div>
 	);
 }
---
+++ src/components/ResultItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ResultItem(props) {
+
+	return (
+		<li className="result__item loan">
+			<div className="loan__intro">
+				<p className="loan__title">{props.loanName}</p>
+				<p className="loan__blurb">Consistent monthly payments over ? years</p>
+			</div>
+			<p className="loan__rate">Interest Rate: {props.loanRate}%</p>
+			<div className="loan__details">
+				<p className="loan__payment">Monthly Payment: ${props.loanPayment}</p>
+				<p className="loan__APR">APR: {props.loanAPR}%</p>
+			</div>
+			<p className="loan__fees">J.G. Wentworth Fees or Credits: ${props.loanFees}</p>
+			<button>I like this loan option</button>
+		</li>
+	)
+}
+
+export default ResultItem;
